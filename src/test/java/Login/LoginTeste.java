c664d49ecb09f47a976f008bf734d4d7f046fc4c
Atila Passos <atilapassos03@gmail.com>
2022-06-02 20:19:25 -0300

    primeiro projeto

---
+++ src/test/java/Login/LoginTeste.java
@@ -0,0 +1,56 @@
+package Login;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTeste {
+
+	WebDriver navegador;
+
+	@Before
+
+	public void openBrowser() {
+
+		// Abrir Navegador;
+
+		// system.setProperty("webdriver.chrome.driver"."C:\\driver\\chormedriver.exe");
+
+		navegador = new ChromeDriver();
+
+		navegador.manage().window().maximize();
+
+	}
+
+	@After
+
+	public void quitBrowser() {
+		// Fechando o navegador
+		// navegador.close();
+		navegador.quit();
+
+	}
+
+	@Test
+
+	public void buscaProduto() {
+
+		// Navegar para a pagina
+
+		navegador.get("http://amazon.com.br");
+
+		Assert.assertEquals("Livros", navegador.findElement(By.xpath("//*[@id=\"nav-xshop\"]/a[5]")).getText());
+
+		// Fazer uma busca por produto
+
+		navegador.findElement(By.id("twotabsearchtextbox")).sendKeys("smartphone");
+
+		navegador.findElement(By.id("nav-search-submit-button")).click();
+
+	}
+
+}
